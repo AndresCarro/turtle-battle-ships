0cefcbc172c56dc407f883219a6b15935662c740
Juan Segundo Arnaude <juansarnaude@gmail.com>
2025-09-07 20:15:46 -0300

    Now saves in the bucket. Fixed .env issues

--- backend/src/data-s3-client.ts
+++ backend/src/data-s3-client.ts
@@ -1,4 +1,9 @@
-import { S3Client } from "@aws-sdk/client-s3";
+import {
+  CreateBucketCommand,
+  HeadBucketCommand,
+  PutBucketPolicyCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
 
 export const s3 = new S3Client({
   region: "us-east-1",
@@ -9,3 +14,31 @@ export const s3 = new S3Client({
   },
   forcePathStyle: true,
 });
+
+const bucketName = process.env.BUCKET_NAME || "game-replays";
+
+export async function createBucketIfNeeded() {
+  try {
+    await s3.send(new HeadBucketCommand({ Bucket: bucketName }));
+  } catch {
+    await s3.send(new CreateBucketCommand({ Bucket: bucketName }));
+
+    const policy = {
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Effect: "Allow",
+          Principal: "*",
+          Action: ["s3:GetObject"],
+          Resource: [`arn:aws:s3:::${bucketName}/*`],
+        },
+      ],
+    };
+    await s3.send(
+      new PutBucketPolicyCommand({
+        Bucket: bucketName,
+        Policy: JSON.stringify(policy),
+      })
+    );
+  }
+}
