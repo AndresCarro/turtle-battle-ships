fbaa7f5560f96c013e60ad90222b2579c2c05b2d
Alejo Flores Lucey <alejo@misterflores.com>
2025-09-10 16:12:14 -0300

    fix

--- react-frontend/src/services/api-utils.ts
+++ react-frontend/src/services/api-utils.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:3000";
+export const API_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
 
 export async function handleResponse<T>(res: Response): Promise<T> {
   if (!res.ok) {
